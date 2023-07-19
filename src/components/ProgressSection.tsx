2f86f00d15cbd60b4676424982b7b43e2be66627
jekl <102255715+jekl0x@users.noreply.github.com>
2023-07-19 19:12:01 -0400

    Update ProgressSection.tsx

--- src/components/ProgressSection.tsx
+++ src/components/ProgressSection.tsx
@@ -74,7 +74,7 @@ export const ProgressSection = ({
             percentage: `${yeetBalance ? softCapDisp : 0}%`,
           },
           {
-            color: "#663333",
+            color: "#FFB224",
             percentage: `${yeetBalance ? hardCapDisp : 0}%`,
           },
         ]}
