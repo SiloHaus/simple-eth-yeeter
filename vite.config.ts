423eb108ee4652ca7ab55ae736e24c556801f6d8
jekl <102255715+jekl0x@users.noreply.github.com>
2023-07-25 15:51:16 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile"
+
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   Object.assign(process.env, loadEnv(mode, process.cwd()));
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), , viteSingleFile()],
     define: {
       "process.env": {
-        NX_RIVET_KEY: process.env.VITE_RIVET_KEY,
+        NX_RIVET_KEY: process.env.VITE_RIVET_KEY,        
+        NX_ALCHEMY_KEY: process.env.VITE_ALCHEMY_KEY,
         NX_GRAPH_API_KEY_MAINNET: process.env.VITE_GRAPH_API_KEY_MAINNET,
         NX_INFURA_PROJECT_ID: process.env.VITE_INFURA_PROJECT_ID,
         NX_ETHERSCAN_KEY: process.env.VITE_ETHERSCAN_KEY,
